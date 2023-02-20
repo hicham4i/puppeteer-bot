b3d2cbf81001b52d141042856e53b07f82b791fa
hicham4i <hicham.rabai@outlook.fr>
2023-02-20 17:08:13 +0100

    update puppeeter viewport

--- routes/html.routes.js
+++ routes/html.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { page } from '../lib/puppeteer.js';
+import { screenBrowser } from '../lib/utils.js';
 
 const router = express.Router();
 
 router.get('/', async (req,res) => {
     const html = await page.content();
-    res.send(html)                 
+    await screenBrowser();
+    res.send(html);               
 });
 export default router
