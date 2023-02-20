4a2a4c21b38ce693598e22d536ec33850a95b646
hicham4i <hicham.rabai@outlook.fr>
2023-02-20 14:24:33 +0100

    beta working bot

--- server.js
+++ server.js
@@ -13,34 +13,71 @@ import pictureRoutes from './routes/pictures.routes.js';
 import txt2imgRoutes from './routes/txt2img.routes.js';
 import img2imgRoutes from './routes/img2img.routes.js';
 import { page } from './lib/puppeteer.js';
-import {  sleep } from './lib/utils.js';
+import {  readAndSplit, screenBrowser, sendImgToJourney, sleep } from './lib/utils.js';
+import { client } from './lib/discord.js';
 
 dotenv.config()
-console.log('🚀 ~~~~~~~~~~~~ server js runing ~~~~~~~~~~~~');
+console.log(`✅ Server is getting started ✅`);
 
 // create express instance
 const app = express()
 
 // set the port for devlopment and for heroku
 const PORT = process.env.PORT || 4001;
+const token = process.env.DISCORD_TOKEN;
 const DISCORD_USER = process.env.DISCORD_USER;
-console.log(`🚀 ~~~~~~~ DISCORD_USER`, DISCORD_USER);
 const DISCORD_PWD = process.env.DISCORD_PWD;
-console.log(`🚀 ~~~~~~~ DISCORD_PWD`, DISCORD_PWD);
+
 await page.goto('https://discord.com/login?redirect_to=%2Fchannels%2F1068479267018641468%2F1068479267480027186', {
     // waitUntil: "load",
     // timeout: 0
     waitUntil: "networkidle0",
 });
-await sleep(2000);
+await sleep(1000);
+await page.waitForSelector('#uid_8');
 await page.type('#uid_8', DISCORD_USER);
 await page.type('#uid_11', DISCORD_PWD?.trim());
-await sleep(2000);
+await sleep(1000);
 await page.click('button[type="submit"] > div', {delay: 10});
-await sleep(2000);
-let filename = `${(new Date().toJSON().slice(0,19).replace(/:/g, '-'))}.png`
-await page.screenshot({path: `./img/screens/${filename}`, captureBeyondViewport: false });
+await sleep(3000);
+await screenBrowser();
+const {
+    solved,
+    error
+} = await page.solveRecaptchas();
+if (solved) {
+    console.log('✔  the captcha is solved')
+} else {
+    console.log('x  the captcha is not solved', error)
+}
+await screenBrowser();
+client.on("ready", async () => {
+    console.log("✅ The AI bot is online ✅"); 
+});
 
+client.on("messageCreate", async (message) => {
+    try {
+        if (message.author.id === '936929561302675456' && message.attachments && message.attachments.first() && message.attachments.first().contentType === 'image/png') {
+            const attachement = message.attachments.first();
+            const ref = message.content.split(',')[1].split('**')[0].trim();
+            readAndSplit(attachement.url, 
+                attachement.name.split('.')[0],
+                message, 
+                message.channelId === '1069981948778385428' ? 'furrytag': message.channelId === '1069981994714415135' ? 'artibar' : 'random',
+                ref
+            );
+            return;
+        }
+        
+        if ( message.channelId === '1069953787235684382' && message.author.id === '1068456688132292648') {
+            const attachement = message.attachments.first();
+            sendImgToJourney(attachement.url, message.content);
+            return;
+        }
+    } catch (error) {
+        console.log(`🚀 ~~~~~~~ client.on ~~~~~~~ error`, error);
+    }
+});
 // Use Express's in-built middleware to parse json and form functionality
 app.use(urlencoded({extended:true}))
 app.use(json())
@@ -68,24 +105,13 @@ app.get('/',(req,res) => {
 
 // listen to port
 app.listen(PORT, async () => {
-    console.log(`Server is running at port ${PORT}`);
-    let close = false;
-    const intv = setInterval( async() => {
-        let filename = `${(new Date().toJSON().slice(0,19).replace(/:/g, '-'))}.png`
-        await page.screenshot({path: `./img/screens/${filename}`, captureBeyondViewport: false });
-        if (close) clearInterval(intv)
-    }, 10000);
-    await sleep(20000);
-    const {
-        solved,
-        error
-    } = await page.solveRecaptchas();
-    
-    if (solved) {
-        console.log('✔  the captcha is solved')
-    } else {
-        console.log('x  the captcha is not solved', error)
-    }
-    close = true;
+    console.log(`✅ Server is running at port ${PORT} ✅`);
+    client.login(token);
+    await screenBrowser();
+    // await readAndSplit('https://media.discordapp.net/attachments/1068479267480027186/1070744623598800976/SEO_ultradetailled_ultrarealistic_3D_cartoon_30246e37-0728-466e-ae63-8c9d48e18fc8.png',
+    // 'azerty',
+    // '',
+    // 'furrytag',
+    // 'test')
 })
 export default app
